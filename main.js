e02c81bfe0cb000d0e91e269e15ec690b4c9e7c9
Robin Dela Cruz <103874277+robin-dc@users.noreply.github.com>
2022-10-12 21:02:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ window.addEventListener("scroll",() =>{
     navbar.classList.remove("nav-active")
     toggle.classList.remove("toggle-active")
 });
+
+const projects = document.querySelector(".btn-project")
+var mq = window.matchMedia('(max-width:1000px)')
+
+function render(e){
+    if(e.matches){
+        projects.innerHTML = `<i class="fa-solid fa-diagram-project"></i>PROJECTS`
+    }
+    else{
+        projects.innerText = "RECENT PROJECTS"
+    }
+}
+render(mq)
+mq.addEventListener('change', render)
